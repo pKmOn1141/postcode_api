338cc0de3b2aa51ea9e503a202b426bc55bc077e
James <james.curzon@capgemini.com>
2026-08-17 14:49:56 +0100

    Validation fixes

--- src/main/java/com/example/postcodeapi/controller/PostcodeController.java
+++ src/main/java/com/example/postcodeapi/controller/PostcodeController.java
@@ -1,9 +1,12 @@
 package com.example.postcodeapi.controller;
 
+import com.example.postcodeapi.model.ResponseError;
+import com.example.postcodeapi.service.InputValidation;
 import com.example.postcodeapi.service.JSONHandling;
 import com.example.postcodeapi.service.PostcodeLookup;
 import com.example.postcodeapi.service.ResponseHandling;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +23,10 @@ public class PostcodeController {
 
     @GetMapping("/postcodes/{postcode}")
     public ResponseEntity<?> getPostcodeResponse(@PathVariable String postcode) {
+
+        if (!InputValidation.isPostcodeValid(postcode)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseError(400, "Invalid postcode format."));
+        }
 
         ResponseEntity<String> pcResponse = postcodeLookup.pcLookup(postcode);
 
